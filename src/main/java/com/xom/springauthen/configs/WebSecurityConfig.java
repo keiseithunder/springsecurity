1fc23a496ee3ea07d0ba34bd066c728e41562dd0
Wicharn Rueangkhajorn <wicharn.keisei@gmail.com>
2023-01-20 10:04:44 +0700

    add user

--- src/main/java/com/xom/springauthen/configs/WebSecurityConfig.java
+++ src/main/java/com/xom/springauthen/configs/WebSecurityConfig.java
@@ -5,16 +5,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
-  
+
   @Bean
   public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().disable();
+    http.cors().and().csrf().disable().authorizeRequests().anyRequest().authenticated().and().formLogin();
     return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+
+    UserDetails user = User.withUsername("user")
+        .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder()::encode)
+        .password("password")
+        .build();
+    return new InMemoryUserDetailsManager(user);
   }
 }
